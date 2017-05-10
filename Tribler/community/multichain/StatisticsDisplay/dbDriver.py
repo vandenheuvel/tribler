4f4cc5cdd83388c0462a4adadd2b1d402886a300
Geert <g.j.habbenjansen@student.tudelft.nl>
2017-05-10 10:55:29 +0200

    Made more changes to the existing node/driver objects.
    These files should now be up to date with my local test project

--- Tribler/community/multichain/StatisticsDisplay/dbDriver.py
+++ Tribler/community/multichain/StatisticsDisplay/dbDriver.py
@@ -1,6 +1,6 @@
 import sqlite3
 
-database_location = "Database/multichain.db"
+default_database = ":memory:"
 
 list_neighbor_query = """
         SELECT DISTINCT hex(public_key_responder) FROM multi_chain where hex(public_key_requester) = ?
@@ -8,21 +8,106 @@ list_neighbor_query = """
         SELECT DISTINCT hex(public_key_requester) FROM multi_chain where hex(public_key_responder) = ?
         """
 
-total_up_query = "SELECT sum(up) FROM multi_chain where hex(public_key_requester) = ?"
+total_self_up_query = "SELECT sum(up) FROM multi_chain where hex(public_key_requester) = ?"
+total_other_down_query = "SELECT sum(down) FROM multi_chain WHERE hex(public_key_responder = ?)"
 
-total_down_query = "SELECT sum(down) FROM multi_chain where hex(public_key_requester) = ?"
+total_self_down_query = "SELECT sum(down) FROM multi_chain where hex(public_key_requester) = ?"
+total_other_up_query = "SELECT sum(up) FROM multi_chain WHERE hex(public_key_responder = ?)"
 
-neighbor_up_query = "SELECT sum(up) FROM multi_chain WHERE hex(public_key_requester) = ? AND hex(public_key_responder) = ?"
+neighbor_self_up_query = "SELECT sum(up) FROM multi_chain WHERE hex(public_key_requester) = ? AND hex(public_key_responder) = ?"
+neighbor_other_down_query = "SELECT sum(down) FROM multi_chain WHERE hex(public_key_responder) = ? AND hex(public_key_requester) = ?"
 
-neighbor_down_query = "SELECT sum(down) FROM multi_chain WHERE hex(public_key_requester) = ? AND hex(public_key_responder) = ?"
+neighbor_self_down_query = "SELECT sum(down) FROM multi_chain WHERE hex(public_key_requester) = ? AND hex(public_key_responder) = ?"
+neighbor_other_up_query = "SELECT sum(up) FROM multi_chain WHERE hex(public_key_responder) = ? AND hex(public_key_requester) = ?"
 
-init_random_focus_query = "SELECT hex(public_key_requester) FROM multi_chain"
+# Create table
+create_table_query = """
+CREATE TABLE IF NOT EXISTS multi_chain(
+ public_key_requester		TEXT NOT NULL,
+ public_key_responder		TEXT NOT NULL,
+ up                         INTEGER NOT NULL,
+ down                       INTEGER NOT NULL,
+
+ total_up_requester         UNSIGNED BIG INT NOT NULL,
+ total_down_requester       UNSIGNED BIG INT NOT NULL,
+ sequence_number_requester  INTEGER NOT NULL,
+ previous_hash_requester	TEXT NOT NULL,
+ signature_requester		TEXT NOT NULL,
+ hash_requester		        TEXT PRIMARY KEY,
+
+ total_up_responder         UNSIGNED BIG INT NOT NULL,
+ total_down_responder       UNSIGNED BIG INT NOT NULL,
+ sequence_number_responder  INTEGER NOT NULL,
+ previous_hash_responder	TEXT NOT NULL,
+ signature_responder		TEXT NOT NULL,
+ hash_responder		        TEXT NOT NULL,
+
+ insert_time                TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
+ )
+"""
+
+# Insert a row of data
+insert_data_query = """
+INSERT INTO multi_chain (public_key_requester, public_key_responder, up, down, total_up_requester, total_down_requester,
+sequence_number_requester, previous_hash_requester, signature_requester, hash_requester, total_up_responder,
+total_down_responder, sequence_number_responder, previous_hash_responder, signature_responder, hash_responder) VALUES
+('0', '1',  10,   5,    0, 0, 0, '', '', 'a', 0, 0, 0, 'a', '', ''),
+('1', '0',  3,    6,    0, 0, 0, '', '', 'b', 0, 0, 0, 'a', '', ''),
+('1', '0',  46,   12,   0, 0, 0, '', '', 'c', 0, 0, 0, 'a', '', ''),
+
+('0', '2',  123,  6,    0, 0, 0, '', '', 'd', 0, 0, 0, 'a', '', ''),
+('2', '0',  21,   3,    0, 0, 0, '', '', 'e', 0, 0, 0, 'a', '', ''),
+
+('0', '3',  22,   68,   0, 0, 0, '', '', 'f', 0, 0, 0, 'a', '', ''),
+('3', '0',  234,  12,   0, 0, 0, '', '', 'g', 0, 0, 0, 'a', '', ''),
+
+('0', '4',  57,   357,  0, 0, 0, '', '', 'h', 0, 0, 0, 'a', '', ''),
+('4', '0',  223,  2,    0, 0, 0, '', '', 'i', 0, 0, 0, 'a', '', ''),
+
+('1', '5',  13,   5,    0, 0, 0, '', '', 'j', 0, 0, 0, 'a', '', ''),
+('5', '1',  14,   6,    0, 0, 0, '', '', 'k', 0, 0, 0, 'a', '', ''),
+
+('1', '6',  234,  5,    0, 0, 0, '', '', 'l', 0, 0, 0, 'a', '', ''),
+
+('1', '10', 102,  5,    0, 0, 0, '', '', 'm', 0, 0, 0, 'a', '', ''),
+('10','1',  123,  0,    0, 0, 0, '', '', 'n', 0, 0, 0, 'a', '', ''),
+
+('2', '7',  87,   5,    0, 0, 0, '', '', 'o', 0, 0, 0, 'a', '', ''),
+('7', '2',  342,  1,    0, 0, 0, '', '', 'p', 0, 0, 0, 'a', '', ''),
+
+('2', '8',  0,    5,    0, 0, 0, '', '', 'q', 0, 0, 0, 'a', '', ''),
+('2', '8',  78,   23,   0, 0, 0, '', '', 'r', 0, 0, 0, 'a', '', ''),
+
+('3', '4',  20,   5,    0, 0, 0, '', '', 's', 0, 0, 0, 'a', '', ''),
+('4', '3',  3,    5,    0, 0, 0, '', '', 't', 0, 0, 0, 'a', '', ''),
+
+('4', '9',  650,  5,    0, 0, 0, '', '', 'u', 0, 0, 0, 'a', '', ''),
+('9', '4',  650,  5,    0, 0, 0, '', '', 'v', 0, 0, 0, 'a', '', ''),
+
+('5', '6',  234,  5,    0, 0, 0, '', '', 'w', 0, 0, 0, 'a', '', ''),
+('6', '5',  5,    323,  0, 0, 0, '', '', 'x', 0, 0, 0, 'a', '', ''),
+
+('6', '7',  12,   5,    0, 0, 0, '', '', 'y', 0, 0, 0, 'a', '', ''),
+('7', '6',  12,   5,    0, 0, 0, '', '', 'z', 0, 0, 0, 'a', '', ''),
+
+('9', '10', 51,   123,  0, 0, 0, '', '', '0', 0, 0, 0, 'a', '', ''),
+('10', '9', 76,   5,    0, 0, 0, '', '', '1', 0, 0, 0, 'a', '', '')
+"""
 
 
 class DbDriver(object):
-    def __init__(self, database=database_location):
-        self.connection = sqlite3.connect(database_location)
+    def __init__(self, database=default_database):
+        self.connection = sqlite3.connect(default_database)
         self.cursor = self.connection.cursor()
+        if database == default_database:
+            print """
+            -----------------
+            USING SAMPLE DATA
+            -----------------
+            """
+            self.cursor.execute(create_table_query)
+            self.cursor.execute(insert_data_query)
+            self.connection.commit()
 
     def neighbor_list(self, focus_node):
         """
@@ -41,8 +126,16 @@ class DbDriver(object):
         :param focus_node: networkNode of the focus
         :return: Num representing the amount of uploaded data
         """
-        self.cursor.execute(total_up_query, (focus_node.public_key,))
-        return self.cursor.fetchone()
+        total = 0
+        self.cursor.execute(total_self_up_query, (focus_node.public_key,))
+        val = self.cursor.fetchone()[0]
+        if val is not None:
+            total += val
+        self.cursor.execute(total_other_down_query, (focus_node.public_key,))
+        val = self.cursor.fetchone()[0]
+        if val is not None:
+            total += val
+        return total
 
     def total_down(self, focus_node):
         """
@@ -50,8 +143,16 @@ class DbDriver(object):
         :param focus_node: networkNode of the focus
         :return: Num representing the amount of downloaded data
         """
-        self.cursor.execute(total_down_query, (focus_node.public_key,))
-        return self.cursor.fetchone()
+        total = 0
+        self.cursor.execute(total_self_down_query, (focus_node.public_key,))
+        val = self.cursor.fetchone()[0]
+        if val is not None:
+            total += val
+        self.cursor.execute(total_other_up_query, (focus_node.public_key,))
+        val = self.cursor.fetchone()[0]
+        if val is not None:
+            total += val
+        return total
 
     def neighbor_up(self, focus_node, neighbor_key):
         """
@@ -60,8 +161,16 @@ class DbDriver(object):
         :param neighbor_key: pulic key of the neighbor's public key
         :return: Num with total upload to neighbor
         """
-        self.cursor.execute(neighbor_up_query, (focus_node.public_key, neighbor_key))
-        return self.cursor.fetchone()[0]
+        total = 0
+        self.cursor.execute(neighbor_self_up_query, (focus_node.public_key, neighbor_key))
+        val = self.cursor.fetchone()[0]
+        if val is not None:
+            total += val
+        self.cursor.execute(neighbor_other_down_query, (focus_node.public_key, neighbor_key))
+        val = self.cursor.fetchone()[0]
+        if val is not None:
+            total += val
+        return total
 
     def neighbor_down(self, focus_node, neighbor_key):
         """
@@ -70,14 +179,13 @@ class DbDriver(object):
         :param neighbor_key: public key of the neighbor's public key
         :return: Num with total download from neighbor
         """
-        self.cursor.execute(neighbor_down_query, (focus_node.public_key, neighbor_key))
-        return self.cursor.fetchone()[0]
-
-    def initCurrentFocus(self):
-        """
-        !!!!    PURELY EXPERIMENTAL    !!!!
-        Get the key of a 'random' node in the database
-        :return: key of one of the nodes
-        """
-        self.cursor.execute(init_random_focus_query)
-        return self.cursor.fetchone()[0]
+        total = 0
+        self.cursor.execute(neighbor_self_down_query, (focus_node.public_key, neighbor_key))
+        val = self.cursor.fetchone()[0]
+        if val is not None:
+            total += val
+        self.cursor.execute(neighbor_other_up_query, (focus_node.public_key, neighbor_key))
+        val = self.cursor.fetchone()[0]
+        if val is not None:
+            total += val
+        return total
--- Tribler/community/multichain/StatisticsDisplay/networkNode.py
+++ Tribler/community/multichain/StatisticsDisplay/networkNode.py
@@ -1,34 +1,29 @@
 class NetworkNode(object):
-
     def __init__(self, public_key, db_driver):
         """
         Creates a networkNode object.
-        
+
         :param public_key: public key of the public key of the node
         :param db_driver: the database driver to use for querying
         """
         self.public_key = public_key
         self.driver = db_driver
         self.neighbor_keys = self.driver.neighbor_list(self)
-        self.total_up = 0
-        self.total_down = 0
+        self.total_uploaded = -1
+        self.total_downloaded = -1
 
     def total_up(self):
-        if self.total_up:
-            return self.total_up
-        else:
-            self.total_up = self.driver.total_up(self)
-            return self.total_up
+        if self.total_uploaded < 0:
+            self.total_uploaded = self.driver.total_up(self)
+        return self.total_uploaded
 
     def total_down(self):
-        if self.total_down:
-            return self.total_down
-        else:
-            self.total_down = self.driver.total_down(self)
-            return self.total_down
+        if self.total_downloaded < 0:
+            self.total_downloaded = self.driver.total_down(self)
+        return self.total_downloaded
 
-    def neighbor_up(self, neighbor_node):
-        return self.driver.neighbor_up(self, neighbor_node)
+    def neighbor_up(self, neighbor_key):
+        return self.driver.neighbor_up(self, neighbor_key)
 
     def neighbor_down(self, neighbor_key):
         return self.driver.neighbor_down(self, neighbor_key)
