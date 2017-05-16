7bf75e138b85f424bbd5760648a74e9ea24b9b7c
Geert <g.j.habbenjansen@student.tudelft.nl>
2017-05-16 09:25:50 +0200

    Removed the @BlockingCallOnReactorThread annotations

--- Tribler/Test/Community/Multichain/statistics/test_statistics_database_new.py
+++ Tribler/Test/Community/Multichain/statistics/test_statistics_database_new.py
@@ -17,7 +17,6 @@ class TestStatisticsDatabase(AbstractServer):
     def __init__(self, *args, **kwargs):
         super(TestStatisticsDatabase, self).__init__(*args, **kwargs)
 
-    @blocking_call_on_reactor_thread
     @inlineCallbacks
     def setUp(self, **kwargs):
         yield super(TestStatisticsDatabase, self).setUp()
@@ -28,17 +27,14 @@ class TestStatisticsDatabase(AbstractServer):
         self.block1 = TestBlock()
         self.block2 = TestBlock()
 
-    @blocking_call_on_reactor_thread
     def test_total_down(self):
         self.db.add_block(self.block1)
         self.assertEqual(self.block1.total_up, self.db.total_up(self.block1.public_key))
 
-    @blocking_call_on_reactor_thread
     def test_total_up(self):
         self.db.add_block(self.block2)
         self.assertEqual(self.block2.total_down, self.db.total_down(self.block2.public_key))
 
-    @blocking_call_on_reactor_thread
     def test_neighbors(self):
         self.block1.link_public_key = self.block2.public_key
         self.db.add_block(self.block1)
