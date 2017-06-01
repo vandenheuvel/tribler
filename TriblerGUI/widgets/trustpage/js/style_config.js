c9ea6b1cbfe775c86b7bd38c4d91318c6c292465
Yinghao Dai <yinghaodai@hotmail.com>
2017-06-01 11:07:36 +0200

    Display quantitative information when hovering over nodes and edges

--- TriblerGUI/widgets/trustpage/js/style_config.js
+++ TriblerGUI/widgets/trustpage/js/style_config.js
@@ -6,16 +6,16 @@ var config = {
     link : {
         colorLinkSource : "#ffff00",
         colorLinkTarget : "#ff0000",
-        strokeWidthMin : 2,
+        strokeWidthMin : 6,
         strokeWidthMax : 10
     },
     node : {
         publicKeyLabel : {
-            color : "#ffff00",
-            fontSize : "12",
+            color : "#000000",
+            fontSize : "14",
             fontFamily : "sans-serif",
-            x : 24,
-            y : 24
+            fontWeight : "bold",
+            characters : 3
         },
         circle : {
             radius : 20,
@@ -26,6 +26,12 @@ var config = {
         color : {
             domain : [0, 0.5, 1],
             range : ["red", "yellow", "green"]
+        },
+        hoverLabel : {
+            publicKeyCharacters : 5,
+            pageRankDecimals : 4,
+            opacityOnNode : 1,
+            opacityOnEdge : 0.8
         }
     },
     radius_step: 120
